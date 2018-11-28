38e512958885cd71b2ab86b9a1cc492be324d517
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-11-28 10:31:11 +0100

    Add conditional rendering examples

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,53 @@
 import React, { Component } from "react";
 
+function longAwardText(movie) {
+  if (!movie.hasOscars) {
+    return <p>Great movie but no Oscars. Rating is {movie.imdbRating}.</p>;
+  }
+  else if (movie.imdbRating >= 9) {
+    return <p>🤯 Amazing film! Won oscars and has a rating of {movie.imdbRating}.</p>;
+  }
+  else if (movie.imdbRating >= 7) {
+    return <p>😯 Got Oscars and has a respectable rating of {movie.imdbRating}.</p>;
+  }
+}
+
+
 class MovieList extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      showOscarFilmsOnly: false,
       movies: [
-        { _id: "7g", title: "The Godfather", director: "Francis Coppola" },
-        { _id: "8h", title: "Star Wars", director: "George Lucas" },
-        { _id: "9i", title: "The Shawshank Redemption", director: "Frank Darabont" },
+        {
+          _id: "7g",
+          title: "The Godfather",
+          director: "Francis Coppola",
+          imdbRating: 9.2,
+          hasOscars: true,
+        },
+        {
+          _id: "8h",
+          title: "Star Wars",
+          director: "George Lucas",
+          imdbRating: 8.7,
+          hasOscars: true,
+        },
+        {
+          _id: "9i",
+          title: "The Shawshank Redemption",
+          director: "Frank Darabont",
+          imdbRating: 9.3,
+          hasOscars: false,
+        },
+        {
+          _id: "10j",
+          title: "Glitter",
+          director: "Vondie Curtis-Hall",
+          imdbRating: 2.2,
+          hasOscars: false,
+        },
       ],
     };
   }
@@ -21,18 +60,41 @@ class MovieList extends Component {
     this.setState({ movies });
   }
 
+  toggleOscarFilter() {
+    const { showOscarFilmsOnly } = this.state;
+    // set the state to the opposite boolean value that it had before
+    this.setState({ showOscarFilmsOnly: !showOscarFilmsOnly });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, showOscarFilmsOnly } = this.state;
 
     // map over the array of object to get "movieHtml" (an array of <li> tags)
     const movieHtml =
       movies.map((oneMovie, index) => {
-        // add a UNIQUE key to the HTML tags you return in "map()"
-        // (this helps React detect what DOM elements to change)
-        return (
+        // show the <li> if the "showOscarFilmsOnly" filter is OFF
+        // -OR- if the filter is ON and the movie has oscars
+        return (!showOscarFilmsOnly || oneMovie.hasOscars) && (
+          // add a UNIQUE key to the HTML tags you return in "map()"
+          // (this helps React detect what DOM elements to change)
           <li key={oneMovie._id}>
             <h3>{oneMovie.title}</h3>
             <p>Directed by {oneMovie.director}</p>
+
+            {/* ↓↓↓ only if the movie won oscars */}
+            {oneMovie.hasOscars ? (
+              <p>Oscar Winning Film 🏆</p>
+            ) : (
+              <p>Great film, but no Oscars. 😒</p>
+            )}
+            {/* ↑↑↑ if the movie DIDN'T win oscars */}
+
+            {/* imdbRating less than 4 */}
+            {oneMovie.imdbRating < 4 &&
+              <p>This movie is 💩.</p>}
+
+            {longAwardText(oneMovie)}
+
             <button onClick={() => this.deleteMovie(index)}>
               Delete
             </button>
@@ -43,6 +105,10 @@ class MovieList extends Component {
     return (
       <section className="MovieList">
         <h2>Movie List Component</h2>
+
+        <button onClick={() => this.toggleOscarFilter()}>
+          Show {showOscarFilmsOnly ? "All Films" : "Oscar Winners Only"}
+        </button>
 
         <ul>
           {/* display your array of HTML elements */}
